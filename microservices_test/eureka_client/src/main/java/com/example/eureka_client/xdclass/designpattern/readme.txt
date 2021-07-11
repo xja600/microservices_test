229d65b6bc226426675944b5caf9746a950550af
xja600 <flyxja2179383>
2021-07-11 18:27:54 +0800

    2021-07-10 设计模式代码提交

--- microservices_test/eureka_client/src/main/java/com/example/eureka_client/xdclass/designpattern/readme.txt
+++ microservices_test/eureka_client/src/main/java/com/example/eureka_client/xdclass/designpattern/readme.txt
@@ -2,4 +2,4 @@
 1、single：简单工厂模式
 2、factory：工厂方法模式
 3、abstractfactory:抽象工厂方法模式
-4、proxypattern：代理模式
+4、proxypattern：代理模式 路径:designpattern.proxypattern.proxy，别的不是
--- microservices_test/eureka_client/src/main/java/com/example/eureka_client/xdclass/designpattern/single/SingletonHungry.java
+++ microservices_test/eureka_client/src/main/java/com/example/eureka_client/xdclass/designpattern/single/SingletonHungry.java
@@ -2,7 +2,11 @@ package com.example.eureka_client.xdclass.designpattern.single;
 
 /**
  * 小滴课堂,愿景：让技术不再难学
- *
+ * 优点：实现简单，没有多线程同步问题
+ * 缺点：不管有没使用，instance对象一直占着这段内存
+ * 或者说：
+ * 优点：简单易读，线程安全
+ * 缺点：可能造成内存浪费(单例的数量大到不可控)
  * @Description 单例设计 饿汉方式
  * @Author 二当家小D
  * @Remark 有问题直接联系我，源码-笔记-技术交流群
@@ -12,6 +16,7 @@ package com.example.eureka_client.xdclass.designpattern.single;
 public class SingletonHungry {
 
     private static SingletonHungry instance = new SingletonHungry();
+    //private static final SingletonHungry instance = new SingletonHungry(); //别的写法
 
     private SingletonHungry(){}
 
