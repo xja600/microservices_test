47212242611ea22c3140e58b5a20b2933bb983f6
xja600 <2369128147@qq.com>
2021-08-17 13:06:08 +0800

    2021-08-17 增加zuul服务模块

---
+++ microservices_test/zuul-service/src/test/java/com/example/zuulservice/ZuulServiceApplicationTests.java
@@ -0,0 +1,15 @@
+/*
+package com.example.zuulservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+*/
