b2143d692082edff637ec5375fb9ef24237620b4
xja600 <2369128147@qq.com>
2021-08-18 18:06:01 +0800

    2021-08-18 增加zuul服务模块

--- microservices_test/zuul-service/src/main/java/com/example/zuulservice/controller/TestController.java
+++ microservices_test/zuul-service/src/main/java/com/example/zuulservice/controller/TestController.java
@@ -9,6 +9,9 @@ import java.util.Date;
 /**
  * #参考文章：https://www.cnblogs.com/q149072205/p/14081185.html
  * 测试zuul
+ * zuul网关其底层使用ribbon来实现请求的路由，并内置Hystrix，可选择性提供网关fallback逻辑。使用zuul的时候，
+ * 并不推荐使用Feign作为application client端的开发实现。毕竟Feign技术是对ribbon的再封装，
+ * 使用Feign本身会提高通讯消耗，降低通讯效率，只在服务相互调用的时候使用Feign来简化代码开发就够了。而且商业开发中，使用Ribbon+RestTemplate来开发的比例更高。
  */
 @RestController
 public class TestController {
