cf6850010b2daac8962f5c8d5a409c6b0b212c7a
xja600 <flyxja2179383>
2022-02-28 22:52:23 +0800

    2022-02-28 加入mybatisplus、swagger

--- microservices_test/springboot_dao/src/main/java/com/spring/dao/SpringbootDaoApplication.java
+++ microservices_test/springboot_dao/src/main/java/com/spring/dao/SpringbootDaoApplication.java
@@ -4,13 +4,16 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
 //@MapperScan({"com.spring.dao.*"})
 //@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 //@MapperScan("com")
 //@ComponentScan(basePackages = { "com.spring.dao","com.slabs"})
-@MapperScan({"com.spring.dao.mapper","com.slabs.pushdata.dao"})
+@MapperScan({"com.spring.dao.mapper","com.slabs.pushdata.dao","com.spring.dao.mybatisplus.mapper",})
+//开启swagger api
+@EnableOpenApi
 public class SpringbootDaoApplication {
 
     public static void main(String[] args) {
